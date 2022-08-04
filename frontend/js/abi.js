e7a6784ef587a251e3b363f1b9b450dea5007607
Ashutosh Ahirwar <ashutoshahirwr@gmail.com>
2022-08-04 13:55:34 +0530

    updated abi line 2 , 3 and last line

--- frontend/js/abi.js
+++ frontend/js/abi.js
@@ -1,6 +1,4 @@
 const abi = [
-    {
-      "inputs": [
         {
           "components": [
             {
@@ -1562,4 +1560,4 @@ const abi = [
       "type": "function",
       "signature": "0x476343ee"
     }
-  ]
+  ];
